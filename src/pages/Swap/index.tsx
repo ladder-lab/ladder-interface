ac9b50513284e93c3238e2bc841ee62b355942e1
cw286 <chiwei@ladder.top>
2022-07-09 21:34:51 +0800

    add setting button

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -1,9 +1,10 @@
 import { useCallback, useState, ChangeEvent } from 'react'
-import { Typography, Box } from '@mui/material'
+import { Typography, Box, IconButton, SxProps } from '@mui/material'
 import AppBody from 'components/AppBody'
 import SelectButton from 'components/Button/SelectButton'
 import NumericalInput from 'components/Input/InputNumerical'
 import ActionButton from 'components/Button/ActionButton'
+import { ReactComponent as SettingIcon } from 'assets/svg/setting.svg'
 
 export default function Swap() {
   const [fromVal, setFromVal] = useState('')
@@ -15,10 +16,11 @@ export default function Swap() {
   return (
     <>
       <AppBody width={'680px'} maxWidth={'680px'}>
-        <Box sx={{ padding: '33px 32px 30px' }}>
+        <Box sx={{ padding: '33px 32px 30px', position: 'relative' }}>
           <Typography fontSize={28} mb={45}>
             SWAP
           </Typography>
+          <SettingButton sx={{ position: 'absolute', right: 32, top: 24 }} />
           <Box display="flex" gap={16}>
             <SelectButton width={'346px'}>DAI</SelectButton>
             <NumericalInput value={fromVal} onChange={onFromVal} maxWidth={254} />
@@ -31,5 +33,13 @@ export default function Swap() {
         </Box>
       </AppBody>
     </>
+  )
+}
+
+function SettingButton({ sx }: { sx: SxProps }) {
+  return (
+    <IconButton sx={{ backgroundColor: '#F7F7F7', borderRadius: '8px', width: 52, height: 52, ...sx }}>
+      <SettingIcon />
+    </IconButton>
   )
 }
