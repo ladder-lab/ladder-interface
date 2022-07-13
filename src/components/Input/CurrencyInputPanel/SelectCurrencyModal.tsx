c476f104cf37a10921ea05d423be64c5b4807df4
cw286 <chiwei@ladder.top>
2022-07-13 19:50:34 +0800

    comment unused

--- src/components/Input/CurrencyInputPanel/SelectCurrencyModal.tsx
+++ src/components/Input/CurrencyInputPanel/SelectCurrencyModal.tsx
@@ -1,39 +1,41 @@
-import { useState, ChangeEvent, useCallback, useEffect } from 'react'
-import { Box, Typography, Button } from '@mui/material'
+import { useState, ChangeEvent, useCallback } from 'react'
+import { Box, Typography } from '@mui/material'
 import Modal from 'components/Modal'
 import CurrencyList from './CurrencyList'
 import Divider from 'components/Divider'
 import Input from 'components/Input'
 import { Currency } from 'constants/token'
+import QuestionHelper from 'components/essential/QuestionHelper'
 
 export enum Mode {
-  SELECT = 'select',
-  IMPORT = 'import'
+  TOKEN = 'token',
+  NFT = 'nft'
 }
 
 export default function SelectCurrencyModal({ onSelectCurrency }: { onSelectCurrency?: (currency: Currency) => void }) {
   const [input, setInput] = useState('')
-  const [mode, SetMode] = useState(Mode.SELECT)
+  const [mode] = useState(Mode.TOKEN)
 
-  const onManage = useCallback(() => {}, [])
+  // const onManage = useCallback(() => {}, [])
 
   const onInput = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value)
   }, [])
 
-  useEffect(() => {
-    if (input !== '') {
-      return SetMode(Mode.IMPORT)
-    }
+  // useEffect(() => {
+  //   if (input !== '') {
+  //     return SetMode(Mode.IMPORT)
+  //   }
 
-    SetMode(Mode.SELECT)
-  }, [input])
+  //   SetMode(Mode.SELECT)
+  // }, [input])
 
   return (
     <>
       <Modal width={'680px'} closeIcon>
-        <Box width="100%" display="flex" padding="32px">
+        <Box width="100%" display="flex" padding="32px" gap={14} alignItems="center">
           <Typography fontSize={24}>Select a token</Typography>
+          <QuestionHelper text="..." size={22} />
         </Box>
         <Box padding="0 32px 23px 32px">
           <Input value={input} onChange={onInput} placeholder="Search by name or paste address" outlined />
@@ -42,12 +44,12 @@ export default function SelectCurrencyModal({ onSelectCurrency }: { onSelectCurr
         <Box paddingTop={'24px'}>
           <CurrencyList mode={mode} currencyOptions={[]} onSelectCurrency={onSelectCurrency} />
         </Box>
-        <Divider />
+        {/* <Divider />
         <Box height="55px" justifyContent="center" display="flex">
           <Button variant="text" onClick={onManage}>
             Manage
           </Button>
-        </Box>
+        </Box> */}
       </Modal>
     </>
   )
