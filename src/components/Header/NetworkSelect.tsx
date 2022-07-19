5bd331222af69027e040af1e202fe98bc02bb3f6
cw286 <chiwei@ladder.top>
2022-07-19 23:28:21 +0800

    update header

--- src/components/Header/NetworkSelect.tsx
+++ src/components/Header/NetworkSelect.tsx
@@ -1,10 +1,11 @@
-import { MenuItem, Box } from '@mui/material'
+import { MenuItem } from '@mui/material'
 import LogoText from 'components/LogoText'
 import Select from 'components/Select/Select'
 import { useActiveWeb3React } from 'hooks'
 import { ChainId, ChainList, SUPPORTED_NETWORKS } from 'constants/chain'
 import useBreakpoint from 'hooks/useBreakpoint'
 import Image from 'components/Image'
+import { theme } from 'theme'
 
 export default function NetworkSelect() {
   const { chainId, account, library } = useActiveWeb3React()
@@ -13,47 +14,45 @@ export default function NetworkSelect() {
   if (!chainId || !account) return null
 
   return (
-    <Box sx={{ width: '130', margin: { xs: '0', sm: '8px 0 15px' } }}>
-      <Select
-        defaultValue={chainId ?? 3}
-        value={chainId ?? 3}
-        width="max-content"
-        height={isDownSm ? '24px' : '36px'}
-        style={{
-          background: 'transparent',
-          border: '1px solid rgba(0, 0, 0, 0.1)',
-          '& .Mui-disabled.MuiSelect-select.MuiInputBase-input': {
-            paddingRight: isDownSm ? 0 : 10,
-            color: theme => theme.palette.text.primary,
-            WebkitTextFillColor: theme => theme.palette.text.primary
-          }
-        }}
-      >
-        {ChainList.map(option => (
-          <MenuItem
-            onClick={() => {
-              if (Object.values(ChainId).includes(option.id)) {
-                library?.provider?.request?.({
-                  method: 'wallet_switchEthereumChain',
-                  params: [{ chainId: SUPPORTED_NETWORKS[option.id as ChainId]?.chainId }, account]
-                })
-              } else {
-                const params = SUPPORTED_NETWORKS[option.id as ChainId]
-                library?.provider?.request?.({ method: 'wallet_addEthereumChain', params: [params, account] })
-              }
-            }}
-            value={option.id}
-            key={option.id}
-            selected={chainId === option.id}
-          >
-            {isDownSm ? (
-              <Image src={option.logo} style={{ height: 14, width: 'auto', margin: '5px 0 0' }} />
-            ) : (
-              <LogoText logo={option.logo} text={option.symbol} gapSize={'small'} fontSize={14} />
-            )}
-          </MenuItem>
-        ))}
-      </Select>
-    </Box>
+    <Select
+      defaultValue={chainId ?? 3}
+      value={chainId ?? 3}
+      width="max-content"
+      height={isDownSm ? '46px' : '44px'}
+      style={{
+        background: theme.palette.background.default,
+        // border: '1px solid rgba(0, 0, 0, 0.1)',
+        '& .Mui-disabled.MuiSelect-select.MuiInputBase-input': {
+          paddingRight: isDownSm ? 0 : 10,
+          color: theme => theme.palette.text.primary,
+          WebkitTextFillColor: theme => theme.palette.text.primary
+        }
+      }}
+    >
+      {ChainList.map(option => (
+        <MenuItem
+          onClick={() => {
+            if (Object.values(ChainId).includes(option.id)) {
+              library?.provider?.request?.({
+                method: 'wallet_switchEthereumChain',
+                params: [{ chainId: SUPPORTED_NETWORKS[option.id as ChainId]?.chainId }, account]
+              })
+            } else {
+              const params = SUPPORTED_NETWORKS[option.id as ChainId]
+              library?.provider?.request?.({ method: 'wallet_addEthereumChain', params: [params, account] })
+            }
+          }}
+          value={option.id}
+          key={option.id}
+          selected={chainId === option.id}
+        >
+          {isDownSm ? (
+            <Image src={option.logo} style={{ height: 20, width: 20, margin: '5px 0 0' }} />
+          ) : (
+            <LogoText logo={option.logo} text={option.symbol} gapSize={'small'} fontSize={14} />
+          )}
+        </MenuItem>
+      ))}
+    </Select>
   )
 }
