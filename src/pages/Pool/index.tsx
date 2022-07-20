f301a4ae957592f7268fa8bbf711d9787d3b9364
cw286 <chiwei@ladder.top>
2022-07-20 21:38:14 +0800

    add AppBody

--- src/pages/Pool/index.tsx
+++ src/pages/Pool/index.tsx
@@ -1,16 +1,36 @@
 import AppBody from 'components/AppBody'
-import { Box, Typography } from '@mui/material'
+import { Box, Typography, useTheme, Button } from '@mui/material'
 
 export default function Pool() {
+  const theme = useTheme()
   return (
-    <AppBody>
-      <Box>
-        <Typography>Liquid provider rewards</Typography>
-        <Typography>
-          Liquidity providers earn a 0.3% fee on all trades proportional to their share of the pool. Fees are added to
-          the pool, accrue in real time and can be claimed by withdrawing your liquidity. Read more about providing
-          liquidity
-        </Typography>
+    <AppBody width={'100%'} maxWidth={'1140px'}>
+      <Box sx={{ padding: '30px 32px' }}>
+        <Box sx={{ padding: '16px 20px', background: theme.palette.background.default, borderRadius: '8px' }}>
+          <Typography sx={{ fontSize: 28, fontWeight: 500, mb: 12 }}>Liquid provider rewards</Typography>
+          <Typography sx={{ fontSize: 18, fontWeight: 500, color: theme.palette.text.secondary }}>
+            Liquidity providers earn a 0.3% fee on all trades proportional to their share of the pool. Fees are added to
+            the pool, accrue in real time and can be claimed by withdrawing your liquidity. Read more about providing
+            liquidity
+          </Typography>
+        </Box>
+        <Box display="flex" justifyContent="space-between" alignItems="center" mt={40}>
+          <Typography sx={{ fontSize: 24 }}>Your Liquidity</Typography>
+          <Box display={'flex'} gap={20}>
+            <Button
+              sx={{
+                fontSize: 12,
+                height: 44,
+                background: theme.palette.background.default,
+                whiteSpace: 'nowrap',
+                minWidth: 'auto'
+              }}
+            >
+              Create a pair
+            </Button>
+            <Button sx={{ fontSize: 12, height: 44, whiteSpace: 'nowrap', minWidth: 'auto' }}>Add Liquidity</Button>
+          </Box>
+        </Box>
       </Box>
     </AppBody>
   )
