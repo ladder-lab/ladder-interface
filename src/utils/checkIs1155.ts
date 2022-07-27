8e34fea2aa4e4bf87c15062a049c6bc39eeb4e57
izz17 <iz17@ladder.top>
2022-07-27 15:46:50 +0800

    implement swap

--- src/utils/checkIs1155.ts
+++ src/utils/checkIs1155.ts
@@ -4,3 +4,10 @@ import { AllTokens } from 'models/allTokens'
 export function checkIs1155(token: AllTokens | null | undefined) {
   return token && token instanceof Token1155 && token.is1155 == true
 }
+
+export function filter1155(token: AllTokens | null | undefined) {
+  if (token && token instanceof Token1155 && token.is1155 == true) {
+    return token
+  }
+  return undefined
+}
