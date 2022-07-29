3aed5dd0e70422aceab0d6ef93b50f99912582a0
izz17 <iz17@ladder.top>
2022-07-29 18:07:25 +0800

    wip pool list

--- src/pages/Pool/index.tsx
+++ src/pages/Pool/index.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
+import { useMemo } from 'react'
 import { useNavigate } from 'react-router-dom'
-import AppBody from 'components/AppBody'
 import { Box, Typography, useTheme, Button, ButtonBase, Grid } from '@mui/material'
+import { Pair } from '@uniswap/sdk'
+import AppBody from 'components/AppBody'
 import { routes } from 'constants/routes'
 import Card from 'components/Card'
 import DoubleCurrencyLogo from 'components/essential/CurrencyLogo/DoubleLogo'
@@ -10,16 +11,51 @@ import Tag from 'components/Tag'
 import CurrencyLogo from 'components/essential/CurrencyLogo'
 import { Dots } from 'theme/components'
 import { Loader } from 'components/AnimatedSvg/Loader'
+import { ExternalLink } from 'theme/components'
+import { toV2LiquidityToken, useTrackedTokenPairs } from 'state/user/hooks'
+import { useTokenBalancesWithLoadingIndicator } from 'state/wallet/hooks'
+import { usePairs } from 'data/Reserves'
+
+import { useActiveWeb3React } from 'hooks'
 
 // Dummy Data
 import { ETHER } from 'constants/token'
-import { ExternalLink } from 'theme/components'
 
 export default function Pool() {
   const theme = useTheme()
   const navigate = useNavigate()
-  const [isLoading] = useState(false)
+  const { account } = useActiveWeb3React()
 
+  // fetch the user's balances of all tracked V2 LP tokens
+  const trackedTokenPairs = useTrackedTokenPairs()
+  const tokenPairsWithLiquidityTokens = useMemo(
+    () => trackedTokenPairs.map(tokens => ({ liquidityToken: toV2LiquidityToken(tokens), tokens })),
+    [trackedTokenPairs]
+  )
+  const liquidityTokens = useMemo(
+    () => tokenPairsWithLiquidityTokens.map(tpwlt => tpwlt.liquidityToken),
+    [tokenPairsWithLiquidityTokens]
+  )
+  const [v2PairsBalances, fetchingV2PairBalances] = useTokenBalancesWithLoadingIndicator(
+    account ?? undefined,
+    liquidityTokens
+  )
+
+  // fetch the reserves for all V2 pools in which the user has a balance
+  const liquidityTokensWithBalances = useMemo(
+    () =>
+      tokenPairsWithLiquidityTokens.filter(({ liquidityToken }) =>
+        v2PairsBalances[liquidityToken.address]?.greaterThan('0')
+      ),
+    [tokenPairsWithLiquidityTokens, v2PairsBalances]
+  )
+
+  const v2Pairs = usePairs(liquidityTokensWithBalances.map(({ tokens }) => tokens))
+  const v2IsLoading =
+    fetchingV2PairBalances || v2Pairs?.length < liquidityTokensWithBalances.length || v2Pairs?.some(V2Pair => !V2Pair)
+
+  const allV2PairsWithLiquidity = v2Pairs.map(([, pair]) => pair).filter((v2Pair): v2Pair is Pair => Boolean(v2Pair))
+  console.log(allV2PairsWithLiquidity)
   return (
     <>
       <AppBody width={'100%'} maxWidth={'1140px'}>
@@ -55,7 +91,7 @@ export default function Pool() {
             </Box>
           </Box>
 
-          {isLoading ? (
+          {v2IsLoading ? (
             <Box minHeight={332} display="flex" justifyContent="center" alignItems="center">
               <Box display="grid" gap={19}>
                 <Typography sx={{ color: theme.palette.text.secondary, fontSize: 20 }}>
@@ -86,7 +122,14 @@ export default function Pool() {
                   tokenAmount="123"
                   shareAmount="456"
                   onAdd={() => navigate(routes.addLiquidity)}
-                  onRemove={() => navigate(routes.removeLiquidity)}
+                  onRemove={() =>
+                    navigate(
+                      routes.removeLiquidity.replace(
+                        '/:address0/:address1/:tokenIdA%tokenIdB',
+                        `/{cur0.address}/{cur1.address}/{filter1155(cur0)?.tokenId??''}%{filter1155(cur1)?.tokenId??''}`
+                      )
+                    )
+                  }
                 />
               </Grid>
             </Grid>
