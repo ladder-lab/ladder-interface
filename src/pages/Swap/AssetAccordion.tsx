cef017eb877824a74257303f3fe5ab1058d392db
izz17 <iz17@ladder.top>
2022-08-10 18:45:40 +0800

    add insufficient amount error to pool

--- src/pages/Swap/AssetAccordion.tsx
+++ src/pages/Swap/AssetAccordion.tsx
@@ -6,6 +6,7 @@ import { AllTokens } from 'models/allTokens'
 import CurrencyLogo from 'components/essential/CurrencyLogo'
 import Tag from 'components/Tag'
 import { checkIs1155 } from 'utils/checkIs1155'
+import Copy from 'components/essential/Copy'
 
 export function AssetAccordion({ token }: { token?: AllTokens }) {
   const [expanded, setExpanded] = useState(false)
@@ -37,8 +38,20 @@ export function AssetAccordion({ token }: { token?: AllTokens }) {
         <CurrencyLogo currency={token} style={{ width: 36 }} />
         <Box display="flex" flexDirection="column" gap={8} width="100%">
           <Typography color={theme.palette.text.secondary}>Name: {token?.name ?? '-'}</Typography>
-          <Typography color={theme.palette.text.secondary} sx={{ wordWrap: 'break-word' }}>
-            Contract: {token && 'address' in token ? token?.address : '-'}
+          <Typography
+            color={theme.palette.text.secondary}
+            component="div"
+            sx={{ wordWrap: 'break-word', display: 'flex', alignItems: 'center', gap: 5 }}
+          >
+            Contract:{' '}
+            {token && 'address' in token ? (
+              <>
+                {token?.address}
+                <Copy toCopy={token?.address} />
+              </>
+            ) : (
+              '-'
+            )}
           </Typography>
           {is1155 && (
             <Typography color={theme.palette.text.secondary} sx={{ wordWrap: 'break-word' }}>
