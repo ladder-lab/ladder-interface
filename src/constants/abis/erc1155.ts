a4f8a5cf22fcf417733337e1e343b56ef92feab2
izz17 <iz17@ladder.top>
2022-08-10 18:16:49 +0800

    add 1155 balances

---
+++ src/constants/abis/erc1155.ts
@@ -0,0 +1,7 @@
+import { Interface } from '@ethersproject/abi'
+import ERC1155_ABI from './erc1155.json'
+
+const ERC1155_INTERFACE = new Interface(ERC1155_ABI)
+
+export default ERC1155_INTERFACE
+export { ERC1155_ABI }
