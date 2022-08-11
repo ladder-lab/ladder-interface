8d7eb2bb002f02a2d126784e2460d1b81008ffae
cw286 <chiwei@ladder.top>
2022-08-11 21:34:38 +0800

    update helpText

--- src/constants/helperText.ts
+++ src/constants/helperText.ts
@@ -1,8 +1,10 @@
 export const HelperText = {
-  expectedOuptut: 'expectedOuptutHelperText',
-  priceImpact: 'priceImpactHelperText',
-  minReceived: 'minReceivedHelperText',
-  networkFee: 'networkFeeHelperText',
+  expectedOuptut:
+    'The amount you expect to receive at the current market price.You may receive less or more if the market price changes while your transaction is pending.',
+  priceImpact: 'The impact your trade has on the market price of this pool.',
+  minReceived:
+    'The minimum amount you are guaranteed to receive.If the price slips any further,your transaction will revert.',
+  networkFee: 'The fee paid to miners who process your transaction.This must be paid in ETH.',
   selectNft: 'selectNft',
   selectToken: 'selectToken'
 }
