21822ee8a41549a74b952e4e645f6b28d61aabba
cw286 <chiwei@ladder.top>
2022-09-04 16:41:03 +0800

    use hook to manage token721 state

--- src/models/allTokens.ts
+++ src/models/allTokens.ts
@@ -4,5 +4,6 @@ import { Token1155 } from 'constants/token/token1155'
 export type AllTokens = Currency | Token1155 | Token
 
 export type NFT = Token1155
+export type Token721 = Token1155 // [TODO] Define Token721
 
-export type TokenType = 'erc1155' | 'erc20'
+export type TokenType = 'erc1155' | 'erc20' | 'erc721'
