3fbd72af386a95269348fed26cd67dd78594d03e
cw286 <chiwei@ladder.top>
2022-09-19 22:00:34 +0800

    update Stepper

--- src/pages/Testnet/Stepper.tsx
+++ src/pages/Testnet/Stepper.tsx
@@ -1,6 +1,5 @@
-import { useCallback } from 'react'
-import { styled } from '@mui/material/styles'
-import { Stepper as MuiStepper, StepLabel } from '@mui/material'
+import { useMemo } from 'react'
+import { Stepper as MuiStepper, StepLabel, Button } from '@mui/material'
 import Step from '@mui/material/Step'
 import StepConnector, { stepConnectorClasses } from '@mui/material/StepConnector'
 import { ReactComponent as CheckIconLight } from 'assets/svg/stepper/checkl.svg'
@@ -10,44 +9,80 @@ import { ReactComponent as CheckIconDark } from 'assets/svg/stepper/checkd.svg'
 import { ReactComponent as LoadingIconDark } from 'assets/svg/stepper/loadingd.svg'
 import { ReactComponent as BgIconDark } from 'assets/svg/stepper/bgd.svg'
 import { useIsDarkMode } from 'state/user/hooks'
-
-const ColorlibConnector = styled(StepConnector)(({ theme }) => ({
-  [`&.${stepConnectorClasses.alternativeLabel}`]: {
-    top: 30
-  },
-  [`& .${stepConnectorClasses.line}`]: {
-    height: 3,
-    border: 0,
-    backgroundColor: theme.palette.background.default,
-    borderRadius: 1
-  }
-}))
-
-const steps = ['LIT-1', 'LIT-2', 'LIT-3', 'LIT-4']
+import { theme } from 'theme'
 
 export default function Stepper() {
   const isDarkMode = useIsDarkMode()
 
-  const getIcon = useCallback(
-    (label: string) => {
-      if (label === steps[0]) {
-        return isDarkMode ? CheckIconDark : CheckIconLight
+  const steps = useMemo(() => {
+    return [
+      {
+        icon: isDarkMode ? CheckIconDark : CheckIconLight,
+        label: 'LIT-1',
+        action: (
+          <Button sx={{ width: 140, height: 51 }} onClick={() => {}} disabled>
+            End
+          </Button>
+        )
+      },
+      {
+        icon: isDarkMode ? LoadingIconDark : LoadingIconLight,
+        label: 'LIT-2',
+        action: (
+          <Button sx={{ width: 140, height: 51 }} onClick={() => {}}>
+            Register
+          </Button>
+        )
+      },
+      {
+        icon: isDarkMode ? BgIconDark : BgIconLight,
+        label: 'LIT-3'
+      },
+      {
+        icon: isDarkMode ? BgIconDark : BgIconLight,
+        label: 'LIT-4'
       }
-
-      if (label === steps[1]) {
-        return isDarkMode ? LoadingIconDark : LoadingIconLight
-      }
-
-      return isDarkMode ? BgIconDark : BgIconLight
-    },
-    [isDarkMode]
-  )
+    ]
+  }, [isDarkMode])
 
   return (
-    <MuiStepper alternativeLabel activeStep={1} connector={<ColorlibConnector />}>
-      {steps.map(label => (
+    <MuiStepper
+      alternativeLabel
+      activeStep={1}
+      connector={
+        <StepConnector
+          sx={{
+            [`&.${stepConnectorClasses.alternativeLabel}`]: {
+              top: 30
+            },
+            [`& .${stepConnectorClasses.line}`]: {
+              height: 6,
+              border: 0,
+              backgroundColor: isDarkMode ? '#1B3536' : '#CEEAEA',
+              borderRadius: 1
+            }
+          }}
+        />
+      }
+    >
+      {steps.map(({ icon, label, action }) => (
         <Step key={label}>
-          <StepLabel StepIconComponent={getIcon(label)}>{label}</StepLabel>
+          <StepLabel
+            sx={{
+              '& .MuiStepLabel-label': {
+                color: theme.palette.text.primary,
+                fontWeight: 700,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                gap: 36
+              }
+            }}
+            StepIconComponent={icon}
+          >
+            {label}
+            {action}
+          </StepLabel>
         </Step>
       ))}
     </MuiStepper>
