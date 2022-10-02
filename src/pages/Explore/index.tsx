47b213d1668ebb00ca665c4c92d64d29adbd5241
cw286 <chiwei@ladder.top>
2022-10-02 23:25:36 +0800

    remove unsued

--- src/pages/Explore/index.tsx
+++ src/pages/Explore/index.tsx
@@ -12,45 +12,6 @@ import BgDark from 'assets/images/bg_dark.png'
 import useBreakpoint from 'hooks/useBreakpoint'
 import { useMemo } from 'react'
 
-const images = [
-  {
-    label: 'San Francisco – Oakland Bay Bridge, United States',
-    imgPath: 'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60'
-  },
-  {
-    label: 'Bird',
-    imgPath: 'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60'
-  },
-  {
-    label: 'Bali, Indonesia',
-    imgPath: 'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250'
-  },
-  {
-    label: 'San Francisco – Oakland Bay Bridge, United States',
-    imgPath: 'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60'
-  },
-  {
-    label: 'Bird',
-    imgPath: 'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60'
-  },
-  {
-    label: 'Bali, Indonesia',
-    imgPath: 'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250'
-  },
-  {
-    label: 'San Francisco – Oakland Bay Bridge, United States',
-    imgPath: 'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60'
-  },
-  {
-    label: 'Bird',
-    imgPath: 'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60'
-  },
-  {
-    label: 'Bali, Indonesia',
-    imgPath: 'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250'
-  }
-]
-
 export default function Explore() {
   const theme = useTheme()
   // const isDarkMode = useIsDarkMode()
