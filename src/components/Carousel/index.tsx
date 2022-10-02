b9f3b2d26673368ae2efd0c5076a70f30851d6b9
cw286 <chiwei@ladder.top>
2022-10-02 17:03:07 +0800

    update Carousel rwd

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import { useTheme, IconButton } from '@mui/material'
 import Box from '@mui/material/Box'
 import MobileStepper from '@mui/material/MobileStepper'
@@ -6,10 +6,23 @@ import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft'
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight'
 import SwipeableViews from 'react-swipeable-views'
 import { autoPlay } from 'react-swipeable-views-utils'
+import useBreakpoint from 'hooks/useBreakpoint'
 
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews)
 
 const images = [
+  {
+    label: 'San Francisco – Oakland Bay Bridge, United States',
+    imgPath: 'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60'
+  },
+  {
+    label: 'Bird',
+    imgPath: 'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60'
+  },
+  {
+    label: 'Bali, Indonesia',
+    imgPath: 'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250'
+  },
   {
     label: 'San Francisco – Oakland Bay Bridge, United States',
     imgPath: 'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60'
@@ -28,39 +41,40 @@ export default function Carousel() {
   const theme = useTheme()
   const [activeStep, setActiveStep] = useState(0)
   const maxSteps = images.length
+  const isDownMd = useBreakpoint('md')
 
-  const handleNext = () => {
-    setActiveStep(prevActiveStep => prevActiveStep + 1)
-  }
-
-  const handleBack = () => {
-    setActiveStep(prevActiveStep => prevActiveStep - 1)
-  }
-
-  const handleStepChange = step => {
-    setActiveStep(step)
-  }
+  const imageSize = useMemo(() => {
+    return {
+      width: isDownMd ? 180 : 380,
+      height: isDownMd ? 218 : 460
+    }
+  }, [isDownMd])
 
   return (
-    <Box sx={{ maxWidth: 520 }}>
+    <Box
+      sx={{
+        width: '100%',
+        maxWidth: 700
+      }}
+    >
       <AutoPlaySwipeableViews
         axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
         index={activeStep}
-        onChangeIndex={handleStepChange}
+        onChangeIndex={(step: number) => setActiveStep(step)}
         enableMouseEvents
-        style={{ padding: '0 60px' }}
-        slideStyle={{ padding: '0 5px' }}
+        style={{ padding: `0 calc((100% - ${imageSize.width}px - 20px) / 2)` }}
+        slideStyle={{ padding: { xs: '0 5px', md: '0 10px' } }}
       >
         {images.map((step, index) => (
           <Box key={step.label}>
             <Box
               component="img"
               sx={{
-                height: 460,
+                height: imageSize.height,
                 display: 'block',
-                maxWidth: 380,
+                maxWidth: imageSize.width,
                 overflow: 'hidden',
-                width: '100%',
+                width: imageSize.width,
                 borderRadius: '24px'
               }}
               src={step.imgPath}
@@ -85,13 +99,13 @@ export default function Carousel() {
         ))}
       </AutoPlaySwipeableViews>
       <MobileStepper
-        sx={{ background: 'transparent', justifyContent: 'center' }}
+        sx={{ background: 'transparent', justifyContent: 'center', mt: 20, padding: 0 }}
         steps={maxSteps}
         position="static"
         activeStep={activeStep}
         nextButton={
           <IconButton
-            onClick={handleNext}
+            onClick={() => setActiveStep(prevActiveStep => prevActiveStep + 1)}
             disabled={activeStep === maxSteps - 1}
             sx={{ background: theme.palette.background.paper, ml: 40 }}
           >
@@ -100,7 +114,7 @@ export default function Carousel() {
         }
         backButton={
           <IconButton
-            onClick={handleBack}
+            onClick={() => setActiveStep(prevActiveStep => prevActiveStep - 1)}
             disabled={activeStep === 0}
             sx={{ background: theme.palette.background.paper, mr: 40 }}
           >
