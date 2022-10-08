6e955ccb30fbf2253f3256327caf3d2714676ea8
cw286 <chiwei@ladder.top>
2022-10-08 12:57:36 +0800

    update StatCard

--- src/pages/Collection/index.tsx
+++ src/pages/Collection/index.tsx
@@ -9,6 +9,7 @@ import BscLogo from 'assets/svg/bsc.svg'
 import { ExternalLink } from 'theme/components'
 import DummyCollectionCover from 'assets/images/dummy_collection_cover.png'
 import { ReactComponent as DummyChart } from 'assets/svg/dummy_chart.svg'
+import { ReactComponent as ShareIcon } from 'assets/svg/share_icon.svg'
 
 export default function Collection() {
   const pairCollectionsData = useMemo(() => {
@@ -99,19 +100,33 @@ export default function Collection() {
 }
 
 function MainCard() {
+  const theme = useTheme()
+
+  const data = useMemo(() => {
+    return {
+      coverImage: DummyCollectionCover,
+      name: 'Tickets for the community #56',
+      address: '0x344dDf5a03AFE67543FB23c674C6797599a0A507',
+      supply: 20000,
+      holder: 500
+    }
+  }, [])
+
   return (
     <Card style={{ height: 698, overflow: 'hidden' }}>
       <Box sx={{ width: '100%', height: 587 }}>
         <Image
           alt="collection-image"
-          src={DummyCollectionCover}
+          src={data.coverImage}
           style={{ width: '100%', height: '100%', objectFit: 'cover' }}
         />
       </Box>
       <Box sx={{ width: 590, height: 111, padding: '20px 24px', display: 'grid', gap: 8 }}>
-        <Typography>Name: Tickets for the community #56</Typography>
-        <Typography>Contract: 0x344dDf5a03AFE67543FB23c674C6797599a0A507</Typography>
-        <Typography>Supply/Holder:20000/500</Typography>
+        <Typography sx={{ color: theme.palette.text.secondary, fontSize: 14 }}>Name: {data.name}</Typography>
+        <Typography sx={{ color: theme.palette.text.secondary, fontSize: 14 }}>Contract: {data.address}</Typography>
+        <Typography sx={{ color: theme.palette.text.secondary, fontSize: 14 }}>
+          Supply/Holder:{data.supply}/{data.holder}
+        </Typography>
       </Box>
     </Card>
   )
@@ -119,46 +134,58 @@ function MainCard() {
 
 function StatCard() {
   const theme = useTheme()
+
+  const data = useMemo(() => {
+    return {
+      collectionType: 'ERC721 Collection',
+      title: 'NM0979 The Truth is Often Somewhere in Between',
+      creator: '1livinginzen',
+      avatar: DummyAvatar
+    }
+  }, [])
+
   return (
     <Card padding="28px 24px" style={{ height: 698 }}>
-      <Box sx={{ width: 590, height: 698 }}>
+      <Box sx={{ height: 698 }}>
         <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
           <Typography sx={{ color: theme.palette.info.main, fontSize: 14, fontWeight: 500 }}>
-            ERC721 Collection
+            {data.collectionType}
           </Typography>
-          <ButtonBase onClick={() => {}}>Share </ButtonBase>
+          <ButtonBase onClick={() => {}}>
+            Share <ShareIcon style={{ marginLeft: 8 }} />
+          </ButtonBase>
         </Box>
-        <Typography variant="h5" sx={{ fontSize: 18, fontWeight: 400, mt: 16, mb: 16 }}>
-          NM0979 The Truth is Often Somewhere in Between
+        <Typography variant="h1" sx={{ fontSize: 18, mt: 16, mb: 16 }}>
+          {data.title}
         </Typography>
         <Box sx={{ display: 'flex', gap: 8 }}>
           <Box sx={{ orderRadius: '50%', width: 40, height: 40 }}>
-            <Image src={DummyAvatar} alt="creator-avatar" style={{ width: '100%', height: '100%' }} />
+            <Image src={data.avatar} alt="creator-avatar" style={{ width: '100%', height: '100%' }} />
           </Box>
           <Box sx={{ display: 'grid', gap: 4 }}>
             <Typography>Creator</Typography>
-            <Typography>1livinginzen</Typography>
+            <Typography>{data.creator}</Typography>
           </Box>
         </Box>
         <Box sx={{ display: 'flex', justifyContent: 'space-between', gap: 16, mt: 21, mb: 44 }}>
-          <Button variant="outlined" sx={{ height: 48 }}>
+          <Button variant="outlined" sx={{ height: 48 }} onClick={() => {}}>
             <LogoText logo={BscLogo} text={'View on BSCscan'} size={'24px'} />
           </Button>
 
-          <Button variant="outlined" sx={{ height: 48 }}>
+          <Button variant="outlined" sx={{ height: 48 }} onClick={() => {}}>
             View accrued fees and analytics
           </Button>
         </Box>
         <Divider />
         <Grid container spacing={10}>
           <Grid item xs={6}>
-            <NumericalCard title="Transations(24hrs)" value="2265" percentage={0.05} />
+            <NumericalCard title="Transations(24hrs)" value="2,265" percentage={0.05} />
           </Grid>
           <Grid item xs={6}>
-            <NumericalCard title="Volume(24hrs)" value="2265" percentage={0.05} />
+            <NumericalCard title="Volume(24hrs)" value="2,265" percentage={0.05} />
           </Grid>
           <Grid item xs={12}>
-            <NumericalCard title="Total Liquidity" value="2265" percentage={0.05} />
+            <NumericalCard title="Total Liquidity" value="2,265" percentage={0.05} />
           </Grid>
         </Grid>
         <Box mt={40} sx={{ width: '100%' }}>
@@ -178,7 +205,9 @@ function NumericalCard({ title, value, percentage }: { title: string; value: str
         </Typography>
         <Box sx={{ display: 'flex', gap: 4, alignItems: 'flex-end' }}>
           <Typography sx={{ color: '#12151B', fontSize: 32, fontWeight: 700 }}>{value}</Typography>
-          <Typography>{percentage * 100}%</Typography>
+          <Typography sx={{ fontSize: 12 }}>
+            {percentage > 0 ? '+' : ''} {percentage * 100}%
+          </Typography>
         </Box>
       </Box>
     </Card>
