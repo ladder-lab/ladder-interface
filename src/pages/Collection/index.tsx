bb7f60f3301571c2ee48ffa70c2268aa9306c51b
cw286 <chiwei@ladder.top>
2022-10-08 12:04:15 +0800

    pair collection data

--- src/pages/Collection/index.tsx
+++ src/pages/Collection/index.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react'
 import { Box, Typography, ButtonBase, Button, Grid, useTheme } from '@mui/material'
 import Card from 'components/Card'
 import Image from 'components/Image'
@@ -5,25 +6,99 @@ import LogoText from 'components/LogoText'
 import Divider from 'components/Divider'
 import DummyAvatar from 'assets/svg/dummy_avatar.svg'
 import BscLogo from 'assets/svg/bsc.svg'
+import { ExternalLink } from 'theme/components'
 
 export default function Collection() {
+  const pairCollectionsData = useMemo(() => {
+    return [
+      {
+        no: 1,
+        collectionType: 'ERC20-ERC1155',
+        title: 'DAI/Tickets for the community #56',
+        liquidity: 12345,
+        liquidity_varies_percentage: 0.3,
+        volume_24h: 12345,
+        volume_7d: 12345,
+        fees: 123,
+        fees_per_liquidity: 0.05
+      },
+      {
+        no: 2,
+        collectionType: 'ERC20-ERC1155',
+        title: 'DAI/Tickets for the community #56',
+        liquidity: 12345,
+        liquidity_varies_percentage: 0.3,
+        volume_24h: 12345,
+        volume_7d: 12345,
+        fees: 123,
+        fees_per_liquidity: 0.05
+      },
+      {
+        no: 3,
+        collectionType: 'ERC20-ERC1155',
+        title: 'DAI/Tickets for the community #56',
+        liquidity: 12345,
+        liquidity_varies_percentage: 0.3,
+        volume_24h: 12345,
+        volume_7d: 12345,
+        fees: 123,
+        fees_per_liquidity: 0.05
+      }
+    ]
+  }, [])
+
   return (
     <>
-      <Box sx={{ display: 'flex', gap: 20, mt: 40 }}>
-        <MainCard />
-        <StatCard />
-      </Box>
-      <Typography>Top pairs</Typography>
-      <Box sx={{ display: 'flex', gap: 20 }}>
-        <PairCard /> <PairCard /> <PairCard />
-      </Box>
+      <Grid container spacing={20} sx={{ padding: '40px 120px' }}>
+        <Grid item xs={6}>
+          <MainCard />
+        </Grid>
+        <Grid item xs={6}>
+          <StatCard />
+        </Grid>
+        <Grid item xs={12}>
+          <Typography>Top pairs</Typography>
+        </Grid>
+        <>
+          {pairCollectionsData.map(
+            (
+              {
+                no,
+                collectionType,
+                title,
+                liquidity,
+                liquidity_varies_percentage,
+                volume_24h,
+                volume_7d,
+                fees,
+                fees_per_liquidity
+              },
+              idx
+            ) => (
+              <Grid key={`collection -${idx}`} item xs={4}>
+                <PairCard
+                  no={no}
+                  collectionType={collectionType}
+                  title={title}
+                  liquidity={liquidity}
+                  liquidity_varies_percentage={liquidity_varies_percentage}
+                  volume_24h={volume_24h}
+                  volume_7d={volume_7d}
+                  fees={fees}
+                  fees_per_liquidity={fees_per_liquidity}
+                />
+              </Grid>
+            )
+          )}
+        </>
+      </Grid>
     </>
   )
 }
 
 function MainCard() {
   return (
-    <Card>
+    <Card style={{ height: 698 }}>
       <Box sx={{ width: 590, height: 587 }}>
         <Image alt="collection-image" src="#" />
       </Box>
@@ -39,7 +114,7 @@ function MainCard() {
 function StatCard() {
   const theme = useTheme()
   return (
-    <Card padding="28px 24px">
+    <Card padding="28px 24px" style={{ height: 698 }}>
       <Box sx={{ width: 590, height: 698 }}>
         <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
           <Typography sx={{ color: theme.palette.info.main, fontSize: 14, fontWeight: 500 }}>
@@ -101,24 +176,68 @@ function NumericalCard({ title, value, percentage }: { title: string; value: str
   )
 }
 
-function PairCard() {
+function PairCard({
+  no,
+  collectionType,
+  // logo1,
+  // logo2,
+  title,
+  liquidity,
+  liquidity_varies_percentage,
+  volume_24h,
+  volume_7d,
+  fees,
+  fees_per_liquidity
+}: {
+  no: number
+  collectionType: string
+  title: string
+  liquidity: number
+  liquidity_varies_percentage: number
+  volume_24h: number
+  volume_7d: number
+  fees: number
+  fees_per_liquidity: number
+}) {
   return (
     <Card padding="20px 17px">
       <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Typography>#1</Typography>
-        <Typography>ERC20-ERC1155</Typography>
+        <Typography>#{no}</Typography>
+        <Typography>{collectionType}</Typography>
       </Box>
       <Card light padding="20px 34px 24px">
         <Box sx={{ display: 'flex' }}>
           {/* <DualLogo /> */}
-          <Typography>DAI/Tickets for the community #56</Typography>
+          <Typography>{title}</Typography>
         </Box>
       </Card>
       <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 4 }}>
         <Typography>Liquidity</Typography>
-        <Typography>$5321 (+33%)</Typography>
+        <Typography>
+          ${liquidity} ({liquidity_varies_percentage}%)
+        </Typography>
       </Box>
-      <Box></Box>
+      <Box sx={{ display: 'flex', flexDirection: 'column', gap: 16 }}>
+        <Box sx={{ display: 'flex', justifyContent: 'spacw-between' }}>
+          <Typography>Volume (24hrs)</Typography>
+          <Typography>{volume_24h}</Typography>
+        </Box>
+        <Box sx={{ display: 'flex', justifyContent: 'spacw-between' }}>
+          <Typography>Volume (7d)</Typography>
+          <Typography>{volume_7d}</Typography>
+        </Box>
+        <Box sx={{ display: 'flex', justifyContent: 'spacw-between' }}>
+          <Typography>Fees (24hrs)</Typography>
+          <Typography>{fees}</Typography>
+        </Box>
+        <Box sx={{ display: 'flex', justifyContent: 'spacw-between' }}>
+          <Typography>1y Fees/ Liquidity</Typography>
+          <Typography>{fees_per_liquidity}</Typography>
+        </Box>
+      </Box>
+      <ExternalLink underline="always" href="#">
+        View accrued fees and analytics
+      </ExternalLink>
     </Card>
   )
 }
