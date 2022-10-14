673af90c6a7b182800512e42e4f6f76bfda77686
izz17 <iz17@ladder.top>
2022-10-14 20:12:23 +0800

    fix redirect

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -17,7 +17,7 @@ import lightBg from 'assets/images/light_bg.png'
 import { useIsDarkMode } from 'state/user/hooks'
 import RemoveLiquidity from './Pool/RemoveLiquidity'
 import Testnet from './Testnet'
-// import Redirect from 'components/Redirect'
+import Redirect from 'components/Redirect'
 
 const AppWrapper = styled('div', { shouldForwardProp: prop => prop !== 'isDarkMode' })<{ isDarkMode: boolean }>(
   ({ theme, isDarkMode }) => ({
@@ -75,7 +75,7 @@ export default function App() {
             <BodyWrapper id="body">
               <Popups />
               <Polling />
-              {/* <Redirect /> */}
+              <Redirect />
               {/* <WarningModal /> */}
               <Web3ReactManager>
                 <Routes>
