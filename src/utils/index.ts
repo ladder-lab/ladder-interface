49fc97658618926ae2e2722be5f30f819fe1a56a
Bill-334 <Bill-33@ladder.top>
2022-10-15 23:05:05 +0800

    feat: new Testnet page

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -146,3 +146,19 @@ export function getRouterContract721(_: number, library: Web3Provider, account?:
 export function escapeRegExp(string: string): string {
   return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&') // $& means the whole matched string
 }
+
+export function addTokenToMetamask(address: string, symbol: string, decimals: number, image = '') {
+  window.ethereum?.request &&
+    window.ethereum?.request({
+      method: 'wallet_watchAsset',
+      params: {
+        type: 'ERC20', // Initially only supports ERC20, but eventually more!
+        options: {
+          address, // The address that the token is at.
+          symbol, // A ticker symbol or shorthand, up to 5 chars.
+          decimals, // The number of decimals in the token
+          image // A string url of the token logo
+        }
+      }
+    })
+}
