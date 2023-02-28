eb3758589c2585ee0f040804eadb7b89e6b33232
mat <mat@@ladder.top>
2023-02-28 17:25:00 +0800

    Edit: sbt ui style

--- src/pages/Earn/Sbt/BecomePartner.tsx
+++ src/pages/Earn/Sbt/BecomePartner.tsx
@@ -7,19 +7,22 @@ import { ReactComponent as Twitter } from 'assets/svg/socials/twitter_square.svg
 import { ReactComponent as Upload } from 'assets/svg/upload.svg'
 import { useState } from 'react'
 import Input from '../../../components/Input'
+import { LineButton, PrimaryButton } from '../../../components/Button/CustomButton'
 
 const ContentWrapper = styled(Box)`
   background-color: white;
   width: 100%;
-  align-items: center;
-  height: 100vh;
+  display: flex;
+  padding-bottom: 115px;
+  flex-direction: column;
 `
 const HeadBox = styled(Box)`
   display: flex;
   width: 100%;
-  height: 437px;
+  flex-direction: column;
   background-color: white;
   justify-content: center;
+  padding: 120px 0 160px 0;
   align-items: center;
   background-image: url(${Head});
 `
@@ -33,11 +36,13 @@ const ContentBox = styled(Box)`
 const RelativeBox = styled(Box)`
   display: flex;
   align-items: center;
+  margin-top: -30px;
   justify-content: center;
 `
 
 const TypeBox = styled(Box)`
   position: relative;
+  width: 49%;
   background-color: #e9f5f5;
   border-radius: 20px;
   padding: 28px 0 0 24px;
@@ -46,9 +51,10 @@ const TypeBox = styled(Box)`
 const RowBox = styled(Box)`
   display: flex;
   flex-direction: row;
+  justify-content: space-between;
 `
 
-function Users({ onSelected }: { onSelected: (type: number) => void }) {
+function Users({ current, onSelected }: { current: number; onSelected: (type: number) => void }) {
   return (
     <TypeBox>
       <BlackText fontSize={20}>Individual users</BlackText>
@@ -56,9 +62,21 @@ function Users({ onSelected }: { onSelected: (type: number) => void }) {
         Individual users submit applications, and we will give priority to the qualifications according to the order of
         application after the individual ladder partner qualifications are open for application
       </BlackText>
-      <Button variant={'outlined'} onClick={() => onSelected(0)}>
-        Selected
-      </Button>
+      {current == 0 ? (
+        <LineButton
+          sx={{ marginTop: '50px', marginBottom: '24px', width: 'fit-content' }}
+          onClick={() => onSelected(0)}
+        >
+          Selected
+        </LineButton>
+      ) : (
+        <PrimaryButton
+          sx={{ marginTop: '50px', marginBottom: '24px', width: 'fit-content' }}
+          onClick={() => onSelected(0)}
+        >
+          Select
+        </PrimaryButton>
+      )}
       <Box sx={{ position: 'absolute', bottom: '0px', right: '0px' }}>
         <Person />
       </Box>
@@ -66,7 +84,7 @@ function Users({ onSelected }: { onSelected: (type: number) => void }) {
   )
 }
 
-function Organization({ onSelected }: { onSelected: (type: number) => void }) {
+function Organization({ current, onSelected }: { current: number; onSelected: (type: number) => void }) {
   return (
     <TypeBox>
       <BlackText fontSize={20}>Organization</BlackText>
@@ -74,9 +92,21 @@ function Organization({ onSelected }: { onSelected: (type: number) => void }) {
         Individual users submit applications, and we will give priority to the qualifications according to the order of
         application after the individual ladder partner qualifications are open for application
       </BlackText>
-      <Button variant={'outlined'} onClick={() => onSelected(1)}>
-        Selected
-      </Button>
+      {current == 1 ? (
+        <LineButton
+          sx={{ marginTop: '50px', marginBottom: '24px', width: 'fit-content' }}
+          onClick={() => onSelected(1)}
+        >
+          Selected
+        </LineButton>
+      ) : (
+        <PrimaryButton
+          sx={{ marginTop: '50px', marginBottom: '24px', width: 'fit-content' }}
+          onClick={() => onSelected(1)}
+        >
+          Select
+        </PrimaryButton>
+      )}
       <Box sx={{ position: 'absolute', bottom: '0px', right: '0px' }}>
         <House />
       </Box>
@@ -132,9 +162,9 @@ function UsersForm() {
   const [inputAddress, setInputAddress] = useState('')
   const [inputName, setInputName] = useState('')
   return (
-    <Grid container>
+    <Grid container spacing={16}>
       <Grid item md={6}>
-        <Button variant={'outlined'}>Connect Wallet</Button>
+        <LineButton>Connect Wallet</LineButton>
       </Grid>
       <Grid item md={6}>
         <TwitterBtn />
@@ -168,14 +198,14 @@ function OrganizationForm() {
   const [inputEmail, setInputEmail] = useState('')
   const [inputIntro, setInputIntro] = useState('')
   return (
-    <Grid container>
+    <Grid container spacing={16}>
       <Grid item md={6}>
         <BlackText>
           <span style={{ color: '#FC1D00' }}>*</span>Upload your organization logo
         </BlackText>
-        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+        <Box sx={{ display: 'flex', alignItems: 'center', marginTop: '10px' }}>
           <Upload />
-          <Button variant={'outlined'}>Upload a photo</Button>
+          <LineButton style={{ marginLeft: '23px', width: 'max-content' }}>Upload a photo</LineButton>
         </Box>
       </Grid>
       <Grid item md={6}>
@@ -235,27 +265,23 @@ export default function BecomePartner() {
   return (
     <ContentWrapper>
       <HeadBox>
-        <Box>
-          <BlackText textAlign={'center'} fontSize={45} alignSelf={'center'}>
-            Ready to Become Ladder Partner?
-          </BlackText>
-          <BlackText textAlign={'center'} fontSize={20}>
-            Thank you for applying to the Ladder Owner SBT. Please fill in the form truthfully and we will keep
-            <br /> the information provided strictly confidential.
-          </BlackText>
-        </Box>
+        <BlackText textAlign={'center'} fontSize={45} alignSelf={'center'} fontWeight={700}>
+          Ready to Become Ladder Partner?
+        </BlackText>
+        <BlackText textAlign={'center'} fontSize={20}>
+          Thank you for applying to the Ladder Owner SBT. Please fill in the form truthfully and we will keep
+          <br /> the information provided strictly confidential.
+        </BlackText>
       </HeadBox>
       <RelativeBox>
-        <ContentBox
-          sx={{ backgroundColor: '#FFFFFF', position: 'absolute', alignSelf: 'center', border: '1px solid #FC1D00' }}
-        >
-          <RowBox>
-            <Users onSelected={setType} />
-            <Organization onSelected={setType} />
+        <ContentBox sx={{ backgroundColor: '#FFFFFF', border: '1px solid #FC1D00' }}>
+          <RowBox mb={60}>
+            <Users current={type} onSelected={setType} />
+            <Organization current={type} onSelected={setType} />
           </RowBox>
           {type == 0 && <UsersForm />}
           {type == 1 && <OrganizationForm />}
-          <Button>Submit</Button>
+          <Button style={{ marginTop: '60px' }}>Submit</Button>
         </ContentBox>
       </RelativeBox>
     </ContentWrapper>
