c171e9082eeb33b7fe049f1432405ad77d1b8030
mat <mat@@ladder.top>
2023-03-01 20:08:20 +0800

    Edit: experience style

--- src/pages/Testnet/V3TaskItem.tsx
+++ src/pages/Testnet/V3TaskItem.tsx
@@ -60,19 +60,38 @@ function TaskItem({
           </Typography>
 
           {completed ? (
-            <Box alignSelf={'end'} display={'flex'} alignItems="center" sx={{ mt: { xs: 10, sm: 0 } }}>
-              <Typography mr={10} fontWeight={600} color={theme.palette.secondary.light}>
-                Completed
-              </Typography>
-              <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M15 4.5L6.75 12.75L3 9"
-                  stroke="#1F9898"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
+            <Box
+              sx={{
+                mt: { xs: 10, sm: 0 },
+                padding: '2px',
+                borderRadius: '12px',
+                background: 'linear-gradient(#C77DFF, #00FBD6)'
+              }}
+            >
+              <Box
+                alignSelf={'end'}
+                display={'flex'}
+                alignItems="center"
+                justifyContent={'center'}
+                sx={{
+                  backgroundColor: 'white',
+                  height: '100%',
+                  borderRadius: '12px'
+                }}
+              >
+                <Typography mr={10} fontWeight={600} color={theme.palette.info.main}>
+                  Completed
+                </Typography>
+                <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M15 4.5L6.75 12.75L3 9"
+                    stroke="#1F9898"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </Box>
             </Box>
           ) : (
             <Box
