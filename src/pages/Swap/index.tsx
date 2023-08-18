2b9208b7eeb13d8c538916f8c9f2f25618b1a4d0
Bill-33 <Bill-33@ladder.top>
2023-08-18 15:42:47 +0800

    feat: update usdt erc20 address

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -39,7 +39,7 @@ import { ExternalLink } from 'theme/components'
 import Image from 'components/Image'
 
 const [currency0, currency1] = [
-  new Token(56, '0x55d398326f99059ff775485246999027b3197955', 18, 'mUSDC', ' MockERC20'),
+  new Token(56, '0x491C966eAd438a76F8992A443049D250fb333337', 18, 'mUSDC', ' MockERC20'),
   new Token721(56, '0xf4F7139b1FcC5Cac2f573Cc4B684Cc75367A9cfD', undefined)
 ]
 
