205895eb720e3fda6a97987154fb5ece5818b650
Bill-33 <Bill-33@ladder.top>
2023-10-12 19:29:35 +0800

    update currency0

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -41,7 +41,8 @@ import Image from 'components/Image'
 import useBreakpoint from 'hooks/useBreakpoint'
 
 const [currency0, currency1] = [
-  new Token(137, '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174', 6, 'USDC', 'USDC'),
+  // new Token(137, '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174', 6, 'USDC', 'USDC'),
+  new Token(137, '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063', 18, 'DAI', 'DAI'),
   new Token721(137, '0x2871e92209D9B0936FbbB178483877f51C7c9321', undefined)
 ]
 
