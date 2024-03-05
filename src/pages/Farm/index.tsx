68459c6085ced45fc2d27105a471c16f793a565d
chenkang <chenshaokang2003@163.com>
2024-03-05 12:35:33 +0800

    update Farm menu style

--- src/pages/Farm/index.tsx
+++ src/pages/Farm/index.tsx
@@ -1,5 +1,6 @@
-import { Box, styled, Typography } from '@mui/material'
+import { Box, styled, Typography, useTheme, Stack, Button } from '@mui/material'
 import { useState } from 'react'
+import useBreakpoint from 'hooks/useBreakpoint'
 import NFTStaking from './NFTStaking'
 import Farms from './Farms'
 
@@ -14,28 +15,28 @@ export const CenterBetweenRow = styled(CenterRow)`
   justify-content: space-between;
 `
 
-const Tab = styled(Row)(({ theme }) => ({
-  background: 'white',
-  width: '100%',
-  padding: '18px 45px',
-  gap: '24px',
-  [theme.breakpoints.down('md')]: {
-    padding: '18px 20px'
-  }
-}))
-const TabItem = styled(Typography)(({ theme }) => ({
-  fontSize: '16px',
-  padding: '12px 24px',
-  cursor: 'pointer',
-  '&.selected': {
-    background: '#1f9898',
-    borderRadius: '12px',
-    color: 'white'
-  },
-  [theme.breakpoints.down('md')]: {
-    padding: '10px 24px'
-  }
-}))
+// const Tab = styled(Row)(({ theme }) => ({
+//   background: 'white',
+//   width: '100%',
+//   padding: '18px 45px',
+//   gap: '24px',
+//   [theme.breakpoints.down('md')]: {
+//     padding: '18px 20px'
+//   }
+// }))
+// const TabItem = styled(Typography)(({ theme }) => ({
+//   fontSize: '16px',
+//   padding: '12px 24px',
+//   cursor: 'pointer',
+//   '&.selected': {
+//     background: '#1f9898',
+//     borderRadius: '12px',
+//     color: 'white'
+//   },
+//   [theme.breakpoints.down('md')]: {
+//     padding: '10px 24px'
+//   }
+// }))
 
 export const Container = styled(Box)(({ theme }) => ({
   width: '100%',
@@ -56,17 +57,72 @@ export enum CardTYPE {
 }
 
 export default function Index() {
+  const theme = useTheme()
+  const isDownMD = useBreakpoint('md')
   const [currentTab, setTab] = useState<number>(0)
   return (
     <>
-      <Tab>
+      {/* <Tab>
         <TabItem onClick={() => setTab(0)} className={currentTab == 0 ? 'selected' : ''}>
-          Farms
+          Farms11
         </TabItem>
         <TabItem onClick={() => setTab(1)} className={currentTab == 1 ? 'selected' : ''}>
           NFT staking
         </TabItem>
-      </Tab>
+      </Tab> */}
+      <Box
+        sx={{
+          overflow: 'hidden',
+          width: '100%',
+          height: '100%',
+          position: 'relative'
+        }}
+      >
+        <Box
+          sx={{
+            background: theme.palette.background.paper,
+            width: '100%',
+            padding: '18px 0'
+          }}
+        >
+          <Stack
+            direction={'row'}
+            flexWrap="wrap"
+            spacing={20}
+            sx={{
+              width: '100%',
+              maxWidth: '1350px',
+              margin: '0 auto'
+            }}
+          >
+            {['Farms', 'NFT staking'].map((item, index) => (
+              <Button
+                sx={{
+                  width: 'fit-content',
+                  height: isDownMD ? 36 : 52,
+                  fontSize: { xs: 13, sm: 16 },
+                  fontWeight: isDownMD ? '500' : 'inherit',
+                  color: currentTab === index ? 'white' : '#343739',
+                  borderRadius: '12px',
+                  background: currentTab === index ? '#1F9898 !important' : 'inherit',
+                  position: isDownMD ? 'relative' : 'inherit',
+                  '&:hover': {
+                    boxShadow: 'unset',
+                    background: currentTab === index ? '#1F9898' : '#F6F6F6'
+                  }
+                }}
+                key={item}
+                onClick={() => {
+                  setTab(index)
+                }}
+                variant={currentTab === index ? 'contained' : 'text'}
+              >
+                {item}
+              </Button>
+            ))}
+          </Stack>
+        </Box>
+      </Box>
       {currentTab == 0 && <Farms />}
       {currentTab == 1 && <NFTStaking />}
     </>
